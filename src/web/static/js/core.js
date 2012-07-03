34cb4bcf7a4fe6aacd359293f5e9f92899521823
David Bialik <david.bialik@fu-berlin.de>
2012-07-03 19:24:23 +0200

    new work for scriptless poimarkers - TODO: prevent auto-opening of infoboxes - TODO: add more content to infobox - TODO: add event-listener to infobox

--- src/web/static/js/core.js
+++ src/web/static/js/core.js
@@ -133,19 +133,58 @@ function addRouteToMap(_routestr) {
 }
 
 function addPointsOfInterestToMap(id) {
-    rand = Math.round(Math.random()*1000);
-    url = 'http://www.userpage.fu-berlin.de/andrez/kml/'+id+'.kml'+"?"+rand;
-    console.log(url);
-    var georssLayer = new google.maps.KmlLayer(url, {preserveViewport: true});
-    
-    georssLayer.setMap(map);
-    
-    google.maps.event.addListener(georssLayer, 'click', function(kmlEvent) {
-    	var html = kmlEvent.featureData.description;
-    	var poi = $(html).find('a').attr('href');
-    	var name = kmlEvent.featureData.name;
-    	triggerTwittersearch(poi, name);
-    });  
+	rand = Math.round(Math.random() * 1000);
+	url = 'http://www.userpage.fu-berlin.de/andrez/kml/' + id + '.kml' + "?" + rand;
+	var urlExists = false;
+
+	// checking if the file exists
+	$.ajax({
+		url : url,
+		type : 'HEAD',
+		error : function() {
+			urlExists = false;
+		},
+		success : function() {
+			console.log(url);
+			urlExists = true;
+		}
+	});
+
+	if (urlExists) {
+		var georssLayer = new google.maps.KmlLayer(url, {
+			preserveViewport : true
+		});
+
+		georssLayer.setMap(map);
+
+		google.maps.event.addListener(georssLayer, 'click', function(kmlEvent) {
+			var html = kmlEvent.featureData.description;
+			var poi = $(html).find('a').attr('href');
+			var name = kmlEvent.featureData.name;
+			triggerTwittersearch(poi, name);
+		});
+
+	} else {
+		console.log("url not reachable: " + url);
+		$(document).ready(
+				function() {
+					var poiDataArray = $("#poilist").children();
+					for ( var poiIndex = 0; poiIndex < poiDataArray.length; poiIndex++) {
+						currentPoi = poiDataArray[poiIndex];
+						currentPoiMeta = $("span > meta[itemprop]", currentPoi);
+						marker = new google.maps.Marker({
+							animation : google.maps.Animation.DROP,
+							map : map,
+							position : new google.maps.LatLng(currentPoiMeta[0].attributes[1].value,
+									currentPoiMeta[1].attributes[1].value)
+						});
+						infoWindow = new google.maps.InfoWindow({
+							content : $("div", currentPoi).html()
+						});
+						infoWindow.open(map, marker);
+					}
+				});
+	}
 }
 
 function triggerTwittersearch(poianchor, title) {
